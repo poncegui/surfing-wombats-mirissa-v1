e0e6062fa5f19b7a7fd7a0a22462e626b480ea08
Poncegui <marta.ponceg@gmail.com>
2023-09-04 21:06:44 +0200

    navbar mobile links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,67 +1,82 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
 import { BsInstagram } from 'react-icons/bs';
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
 import { FaFacebookF } from 'react-icons/fa';
-import { HiOutlineBars3 } from "react-icons/hi2";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
+import { HiOutlineBars3 } from 'react-icons/hi2';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { Link } from 'react-router-dom';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import LocalBarIcon from '@mui/icons-material/LocalBar';
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
     {
-      text: "Home",
+      text: 'Home',
       icon: <HomeIcon />,
+      href: '/home',
     },
     {
-      text: "About",
+      text: 'About',
       icon: <InfoIcon />,
+      href: '/home',
     },
     {
-      text: "Menu",
+      text: 'Menu',
       icon: <RestaurantMenuIcon />,
+      href: '/home',
     },
     {
-      text: "Drinks & Coctails",
+      text: 'Drinks & Cocktails',
       icon: <LocalBarIcon />,
+      href: '/home',
     },
     {
-      text: "Bookings by DM",
+      text: 'Bookings by DM',
       icon: <InstagramIcon />,
+      href: 'https://www.instagram.com/surfingwombatsmirissa/?hl=es',
+    },
+    {
+      text: 'Follow Us' ,
+      icon: <FaFacebookF />,
+      href: 'https://www.facebook.com/profile.php?id=10007809358703',
     },
   ];
   return (
     <nav>
       <div className="navbar-links-container">
-        <a href="">| Home |</a>
-        <a href=""> | About  |</a>
-        <a href=""> | Menu  |</a>
-        <a href=""> | Cocktails  |</a>
-        <a href=""> | Contact  |</a>
-         <a href="https://www.facebook.com/profile.php?id=10007809358703">
+        <a href="/home">| Home |</a>
+        <a href="/about"> | About |</a>
+        <a href=""> | Menu |</a>
+        <a href=""> | Cocktails |</a>
+        <a href="/contact"> | Contact |</a>
+        <a href="https://www.facebook.com/profile.php?id=10007809358703">
           <FaFacebookF className="navbar-cart-icon" />
-        </a> 
+        </a>
         {/* <a href="">
           <BsInstagram className="navbar-cart-icon" />
         </a>  */}
-        <Link to="https://www.instagram.com/surfingwombatsmirissa/?hl=es" target="_blank" rel="noreferrer">
-        <button className="primary-button"><InstagramIcon className="navbar-cart-icon" /> ∼  Bookings by DM </button>
+        <Link
+          to="https://www.instagram.com/surfingwombatsmirissa/?hl=es"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="primary-button">
+            <InstagramIcon className="navbar-cart-icon" /> ∼ Bookings by DM{' '}
+          </button>
         </Link>
       </div>
       <div className="navbar-menu-container">
@@ -75,11 +90,13 @@ const Navbar = () => {
           onKeyDown={() => setOpenMenu(false)}
         >
           <List>
-            {menuOptions.map((item) => (
+            {menuOptions.map(item => (
               <ListItem key={item.text} disablePadding>
                 <ListItemButton>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                  <a to={item.href} target="_blank" rel="noreferrer">
+                    <ListItemText primary={item.text} />
+                  </a>
                 </ListItemButton>
               </ListItem>
             ))}
@@ -92,3 +109,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
