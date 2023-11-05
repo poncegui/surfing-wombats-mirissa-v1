c5db3495cd0a3e42b7335ba8082e29ea9d7a0718
Poncegui <marta.ponceg@gmail.com>
2023-11-05 10:08:13 +0100

    modify titles

--- src/Components/components/HeadingSection/HeadingSection.jsx
+++ src/Components/components/HeadingSection/HeadingSection.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const HeadingSection = ({ title }) => {
   return (
-      <HeadingStyles>
-        <h2>{title}</h2>
-      </HeadingStyles>
+    <HeadingStyles>
+      <h2>{title}</h2>
+    </HeadingStyles>
   );
 };
 export default HeadingSection;
@@ -13,25 +13,27 @@ export default HeadingSection;
 const HeadingStyles = styled.div`
   border-radius: 5px;
   margin: 30px 0;
-  background-image: url("./assets/title_pattern.png");
+  background-image: url('./assets/title_pattern.png');
   height: 50px;
   background-position: center;
-  margin-top:15%;
+  margin-top: 15%;
 
   h2 {
-  font-weight: 400;
-  background: #B95D5B;
-  color: #f9efe4;
-  height: 50px;
-  width: 100%;
-  text-align: center;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  font-size: 24px;
-}
-  @media  (max-width: 768px) {
+    font-weight: 400;
+    background: #b95d5b;
+    color: #f9efe4;
+    height: 50px;
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    font-size: 24px;
 
-
-}
+    @media (max-width: 768px) {
+      background-color: transparent;
+      color: #b95d5b;
+      font-size: 28px;
+    }
+  }
 `;
